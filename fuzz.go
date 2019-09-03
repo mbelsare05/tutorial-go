f552a93cf128ff1ce07bb9d9c8e217e753732c80
Mohit <mohit.belsare@druva.com>
2019-09-03 14:34:00 +0530

    Added ZLib test

--- fuzz.go
+++ fuzz.go
@@ -3,8 +3,6 @@ package tutorial
 // FuzzerEntrypoint is the method Fuzzbuzz will repeatedly
 // run with new tests to try and find bugs in BrokenMethod
 func FuzzerEntrypoint(Data []byte) int {
-	// Convert Data to the type we need:
-	testString := string(Data)
-	BrokenMethod(testString)
+	Fuzz(Data)
 	return 0
 }
--- method.go
+++ method.go
@@ -1,11 +1,49 @@
 package tutorial
 
-// BrokenMethod has a bug - it will try to read the 4th
-// index of Data even when it only has a length of 3.
-func BrokenMethod(Data string) bool {
-	return len(Data) >= 3 &&
-		Data[0] == 'F' &&
-		Data[1] == 'U' &&
-		Data[2] == 'Z' &&
-		Data[3] == 'Z'
+import (
+	"bytes"
+	szlib "compress/zlib"
+	"github.com/klauspost/compress/zlib"
+	"io/ioutil"
+)
+
+func Fuzz(data []byte) int {
+	fr, err := szlib.NewReader(bytes.NewReader(data))
+	if err != nil {
+		return 0
+	}
+	data1, err := ioutil.ReadAll(fr)
+	if err != nil {
+		return 0
+	}
+	for level := 0; level <= 9; level++ {
+		buf := new(bytes.Buffer)
+		fw, err := zlib.NewWriterLevel(buf, level)
+		if err != nil {
+			panic(err)
+		}
+		n, err := fw.Write(data1)
+		if n != len(data1) {
+			panic("short write")
+		}
+		if err != nil {
+			panic(err)
+		}
+		err = fw.Close()
+		if err != nil {
+			panic(err)
+		}
+		fr1, err := zlib.NewReader(buf)
+		if err != nil {
+			panic(err)
+		}
+		data2, err := ioutil.ReadAll(fr1)
+		if err != nil {
+			panic(err)
+		}
+		if bytes.Compare(data1, data2) != 0 {
+			panic("not equal")
+		}
+	}
+	return 1
 }
